2c5466754c2cead4cb4c5d5ebddda07752f3e983
Uday-Amuru <148942931+Uday-Amuru@users.noreply.github.com>
2026-02-26 14:34:04 +0530

    professional

---
+++ src/components/Basics.tsx
@@ -0,0 +1,9 @@
+export default function Basic() {
+    return (
+        <main className="pt-24">
+        <div className="p-10 text-3xl font-bold">
+            dfjwhdfjkhhhhhhhhdlj
+        </div>
+        </main>
+    );
+}
---
+++ src/components/Professional.tsx
@@ -0,0 +1,239 @@
+export default function Professional() {
+    return (
+        <main className="pt-24">
+            <h1 className="p-10 text-3xl bg-[#ecf0f1] text-center text-green-900">Cosmetology courses</h1>
+            <div className="p-10">
+
+                <h1>Cosmetology Course Details</h1>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                    <div className="p-4 bg-white shadow rounded">
+                        <h1>Section-1</h1>
+                        <h2>Introduction</h2>
+                        <li> Introduction to Aesthetic Dermatology</li>
+                        <li> Anatomy & Physiology of skin</li>
+                        <li> Skin types</li>
+                        <li> Skin Analysis & Analysers</li>
+                        <li> Role of Anaesthesia</li>
+                        <li> Minor Procedures Electrosurgery
+
+                            – Cryosurgery
+
+                            – Dermabrasion
+
+                            – Surgical removal of com</li>
+                        <li> Hpertrophie scars & Keloids</li>
+
+                    </div>
+
+                    <div className="p-4 bg-white shadow rounded">
+                        <p>Section-2</p>
+                        <h1>Basic Cosmetology</h1>
+                        <li> Cosmeceuticals</li>
+                        <li>Personal care & cosmetic products</li>
+                        <li>Household cosmetics</li>
+                        <li>Cosmetic care & cosmelling for normal & problemative skin</li>
+                        <li>Moisturizers & sunscreen</li>
+                        <li>Adverse reaction to cosmetics</li>
+
+                    </div>
+                    <div className="p-4 bg-white shadow rounded">
+                        <p>Section-3</p>
+                        <h1>Clinical Cosmetology</h1>
+                        <li>Acne & its scars</li>
+                        <li>Pigmentary disorders</li>
+                        <li>Cosmetic problems of nails</li>
+                        <li>Hyperhidrosis – Bromohidrosis – Chromohidrosis</li>
+                        <li>Ageing skin problems</li>
+
+
+
+                    </div>
+                    <div className="p-4 bg-white shadow rounded">
+                        <p>Section-4</p>
+                        <h1>Special Surgical Procedures</h1>
+                        <li> Botulinum toxin</li>
+                        <li>Dermal fillers</li>
+                        <li>Thread lifting</li>
+                        <li>Ear lobe repair</li>
+                        <li>Ear & nose piercing</li>
+                        <li>Phototherapy</li>
+                        <li> Electroepilation</li>
+                        <li>Mole surgery</li>
+                    </div>
+                    <div className="p-4 bg-white shadow rounded">
+                        <p>Section-5</p>
+                        <h1>Chemicals Peels</h1>
+                        <li> Introduction</li>
+                        <li>Basic chemistry </li>
+                        <li>Depth of peels </li>
+                        <li>Indication & contraindication</li>
+                        <li> Pre & post procedure care & instruction</li>
+                        <li> Side effects & complications</li>
+                    </div>
+                    <div className="p-4 bg-white shadow rounded">
+                        <p>Section-6</p>
+                        <h1>Lasers</h1>
+                        <li>Introduction</li>
+                        <li>Laser hair removal</li>
+                        <li>Skin resurfacing</li>
+                        <li>Vascular lesions</li>
+                        <li>Pigmented lesions</li>
+                        <li>Skin tightening</li>
+                        <li>Fat reduction & body contouring</li>
+                    </div>
+                    <div className="p-4 bg-white shadow rounded">
+                        <p>Section-7</p>
+                        <h1>Botulinum / Fillers</h1>
+                        <li>Introduction</li>
+                        <li>H/O clinical application of Botox /Fillers</li>
+                        <li>Pharmacology & action of neurotoxin </li>
+                        <li>Clinical application of botulinum toxin/ Fillers in aesthetic dermatology</li>
+                        <li> Adverse effects & their treatment </li>
+                        <li>Special considerations.</li>
+                    </div>
+                    <div className="p-4 bg-white shadow rounded">
+                        <p>Section-8</p>
+                        <h1>Soft Tissue Augmentation</h1>
+                        <li>Introduction </li>
+                        <li>Indications</li>
+                        <li>Techniques of injection</li>
+                        <li>Types of fillers</li>
+                        <li>Common injection errors </li>
+                        <li>Adverse reactions & their treatments</li>
+                    </div>
+                    <div className="p-4 bg-white shadow rounded">
+                        <p>Section-9</p>
+                        <h1>Trichology</h1>
+                        <li>Anatomy & physiology of hair</li>
+                        <li>Hair analysis</li>
+                        <li>FUE Hair Implantation</li>
+                        <li>Scalp Micro Pigmentation</li>
+                        <li> Growth Factors</li>
+                        <li>Eye Brow restoration</li>
+                        <li>Beard & Mustache Restoration</li>
+                        <li> Scar Repait</li>
+                        <li>Hair Prosthetics</li>
+                        <li>.Common disorders o hair – Non scarring alopecia
+
+                            – Dandruff
+
+                            – Hirsutism
+
+                            – Androgenic alopecia
+
+                            – Female Pattern Hair loss</li>
+                        <li>Mesotherapy</li>
+
+                    </div>
+                    <div className="p-4 bg-white shadow rounded">
+                        <p>Section-10</p>
+                        <h1>Well Ness</h1>
+                        <li>Diet for acne</li>
+                        <li>Diet for hairfall</li>
+                        <li>Diet for Healthy & Glowing skin</li>
+                        <li>Basic skin care</li>
+                        <li> Natural approach to beautiful skin , hair & nails</li>
+                        <li> Skin stressors</li>
+                        <li>Harmones & skin</li>
+                        <li>Skin & weather</li>
+                        <li>Skin care essentials</li>
+                        <li>Skin care emergencies</li>
+                    </div>
+                    <div className="p-4 bg-white shadow rounded">
+                        <p>Section-11</p>
+                        <h1>Botulinum Toxin/ Fillers(Upper, Lower & Middle Face)</h1>
+                        <li> Module Based reference materials and reference Reading materials from International authors on Dermatology & Cosmetology in the print and Pdf form.</li>
+                        <li>Basic Reading materials from Standard Books and journals</li>
+                        <li> Videos and Talks on relevant procedures in DVD</li>
+                        <li>Suggestions on Clinic setup, advertisement and Marketing/ Management/ Franchise / Job Placement Assistance</li>
+                        <li> Course Completion Certificate</li>
+
+                    </div>
+                </div>
+
+                <h1 className="text-2xl p-8">
+                    Cosmetology Course Fees
+                </h1>
+
+
+                <div className="overflow-x-auto">
+                    <table className="w-full border border-gray-300">
+                        <thead className="bg-gray-100">
+                            <tr>
+                                <th className=" p-3">S.No</th>
+                                <th className="p-3">Name</th>
+                                <th className="p-3">Cost</th>
+                                <th className="p-3">Time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className=" p-3 text-center">1</td>
+                                <td className=" p-3">Basic Dermatology & Lasers</td>
+                                <td className="p-3">60,000 + tax</td>
+                                <td className=" p-3">15 Days or 4 Weekends (Flexible weekdays)</td>
+                            </tr>
+
+                            <tr>
+                                <td className=" p-3 text-center">2</td>
+                                <td className=" p-3">Basic Dermatology Without Lasers</td>
+                                <td className="p-3">40,000 + tax</td>
+                                <td className="p-3">15 Days or 4 Weekends (Flexible weekdays)</td>
+                            </tr>
+
+                            <tr>
+                                <td className="p-3 text-center">3</td>
+                                <td className="p-3">Botox & Fillers</td>
+                                <td className="p-3">
+                                    50,000 + tax + 20,000 (material charges for one hand-on)
+                                </td>
+                                <td className="p-3">
+                                    One Week or 2 weekends or flexible days
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td className=" p-3 text-center">4</td>
+                                <td className=" p-3">Botox, fillers & threadlift</td>
+                                <td className=" p-3">
+                                    70,000 + tax, 1L (with materials & 1 hands on)
+                                </td>
+                                <td className=" p-3">
+                                    One Week or 2 weekends or flexible days
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td className=" p-3 text-center">5</td>
+                                <td className=" p-3">Any exclusive course</td>
+                                <td className="p-3">
+                                    Only CP - 10,000 + tax, Only Lasers - 30,000 + tax
+                                </td>
+                                <td className=" p-3">
+                                    One Week or 2 weekends or flexible days
+                                </td>
+                            </tr>
+
+                            <tr className="bg-gray-50 font-semibold">
+                                <td className=" p-3 text-center">6</td>
+                                <td className=" p-3">
+                                    All courses (Dermatology + Cosmetology)
+                                </td>
+                                <td className=" p-3">
+                                    1,50,000 + Material charges
+                                </td>
+                                <td className="border border-gray-300 p-3">
+                                    One Month Full time or 10 Weekends
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </main>
+    );
+}
+
+    
